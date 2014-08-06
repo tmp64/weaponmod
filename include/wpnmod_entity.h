cca1ae16ad832a72118ae869c2e31d7786ff1da5
KORD <KORD@4bd4d9a2-232f-344e-8b09-93cf19f43987>
2014-08-06 13:07:44 +0000

    Why so serious, linux?
    git-svn-id: https://svn.aghl.ru:8443/svn/HLModules/weaponmod/trunk@253 4bd4d9a2-232f-344e-8b09-93cf19f43987

--- include/wpnmod_entity.h
+++ include/wpnmod_entity.h
@@ -137,7 +137,7 @@ public:
 
 			for (int i = 0; i < (int)pFields.size(); i++)
 			{
-				if (!_strcmpi(pFields[i]->pKey, pKey))
+				if (!strcmp(pFields[i]->pKey, pKey))
 				{
 					return pFields[i]->pValue;
 				}
