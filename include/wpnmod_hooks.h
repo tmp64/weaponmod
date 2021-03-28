6ae6da612f3cfd13c7290324cfd87e12eace6e5b
tmp64 <tmp32x2@gmail.com>
2021-03-28 17:46:32 +0700

    Undo APPLY_MULTI_DAMAGE changes

--- include/wpnmod_hooks.h
+++ include/wpnmod_hooks.h
@@ -80,7 +80,7 @@
 	//
 		inline void APPLY_MULTI_DAMAGE(edict_t* pentInflictor, edict_t* pentAttacker)
 		{
-			reinterpret_cast<FuncApplyMultiDamage>(g_Memory.m_pApplyMultiDamage)(&(entvars_t)pentInflictor->v, &(entvars_t)pentAttacker->v);
+			reinterpret_cast<FuncApplyMultiDamage>(g_Memory.m_pApplyMultiDamage)(VARS(pentInflictor), VARS(pentAttacker));
 		}
 
 	// void W_Precache(void)
